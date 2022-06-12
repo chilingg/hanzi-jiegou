1848260aafba2b58d8d3389f1f439e0be637bd99
chilingg <chilingg@qq.com>
2022-06-13 01:49:41 +0800

    实践并修正(1/4)

--- scripts/genGlyphTab.py
+++ scripts/genGlyphTab.py
@@ -24,21 +24,25 @@ def genGlyphTab(file = 'glyph.json'):
     ch.init()
     glyphTab = dict()
 
-    lrSmall2 = '乚丨乙八卜匕冫厂刁丁刂刀儿二阝丩几九卩㔾丂力了七人入亻十乂讠又'
+    lrSmall2 = '乚丨乙卜匕冫厂刁丁刂刀儿二阝丩几九卩㔾丂力了七人入亻十乂讠又'
     lrSmall3 = '才叉川寸彳亍巛大凡飞干个工广弓及己巾久彐孑孓口亏么马门女丬乞千犭刃夊三上勺尸士巳扌彡饣氵纟山土乇丸万亡卫兀卂习下乡小忄夕丫也已义于与弋幺丈之子夂'
-    lrSmall4 = '币卞不刅长尺丑歹斗方分夫父丰丐户火见斤今巨井开亢六仑木牜廿牛内匹片爿攴攵气欠区犬壬冗日少升手殳礻水太天韦文无五午王冘牙尹尤友予匀允夭爻元月云支止中爪专㝉曰'
-    lrSmall5 = '衤钅石禾玉田白目示未立甘生米糸页羊舟虫耒耳肉足言辛圭'
+    lrSmall4 = '币卞不刅长尺丑歹斗方分夫父丰丐户火见斤巨井开亢六仑木牜廿牛内匹片爿攴攵气欠区犬壬冗日少升手殳礻水太天韦文无五午王冘牙尹尤友予匀允夭爻元月云支止中专曰贝车半宁介'
+    lrSmall5 = '衤钅石禾玉田白目示未立甘生米糸页羊舟虫耒耳肉足言辛圭舌享古求争交正革巴邑百豸𠬝关矢鸟乌骨皀毛冎丘吕戈且旦豆缶'
     lrSmall = lrSmall2 + lrSmall3 + lrSmall4 + lrSmall5
+    lrSmalls = [ '归-彐', '印-卩', '新-斤' ]
 
-    tbSmall1 = '一卜二冖十厶亠艹口宀三上士工山土亡小牛亼灬冃㓁爫㣺'
-    tbSmall2 = '乙八匕丁儿几九㔾了七人入川大干巾久乇万兀下丫已于之子巳不木廿壬午止𣎵廾'
+    tbZero = '一冖亠宀丿艹㓁爫⺮罒人卜'
+    bZero = '一灬'
+    tbSmall1 = '二厶十卜口三上士工山土亡小少牛亼冃㣺心穴𠫓日曰屮囗巛巾' + tbZero + bZero[1:]
+    tbSmall2 = '乙八匕丁儿几九㔾了七入川大干久乇万兀下卞丫已于之子巳不廿壬午止𣎵廾木雨皿又犬寸石文艸白䜌攵田丌力𤇾夂且火去比王女玉林从立臤羽臼'
     tbSmall = tbSmall1 + tbSmall2
+    tbSmalls = [ '益-皿', '学-子', '高-冋', '甾-田' ]
 
-    ltSmall = '广厃又尸厂疒毛倝歹㫃才'
+    ltSmall = '广厃又尸厂疒毛倝歹㫃才户仁'
 
-    ltrSmall = '门戊凡几目戉冂戌宀网戕产'
+    ltrSmall = '门凡几目冂宀网产戊'
 
-    thSmall = '癶八人入乃六廾'
+    thSmall = '癶八人入乃六廾大'
 
     def ratioToFormat(format, comps):
         small1 = False
@@ -47,8 +51,21 @@ def genGlyphTab(file = 'glyph.json'):
         ratio = ''
         
         if format == '左右':
-            small1 = isinstance(comps[0], str) and lrSmall.count(comps[0][0])
-            small2 = isinstance(comps[1], str) and lrSmall.count(comps[1][0])
+            if isinstance(comps[0], str):
+                if comps[0][1:2] == '-':
+                    small1 = lrSmalls.count(comps[0])
+                else:
+                    small1 = lrSmall.count(comps[0][0])
+            elif comps[0]['format'] == '上下':
+                    small1 = lrSmall.count(comps[0]['components'][0]) and lrSmall.count(comps[0]['components'][1])
+            if isinstance(comps[1], str):
+                if comps[1][1:2] == '-':
+                    small2 = lrSmalls.count(comps[1])
+                else:
+                    small2 = lrSmall.count(comps[1][0])
+            elif comps[1]['format'] == '上下':
+                    small2 = lrSmall.count(comps[1]['components'][0]) and lrSmall.count(comps[1]['components'][1])
+
             if small1 == small2:
                 ratio = '(1：1)'
             elif small1:
@@ -68,15 +85,29 @@ def genGlyphTab(file = 'glyph.json'):
                 if comps[0]['format'] == '左右':
                     small1 = tbSmall.count(comps[0]['components'][0]) and tbSmall.count(comps[0]['components'][1])
             else:
-                small1 = tbSmall.count(comps[0][0])
+                if comps[0][1:2] == '-':
+                    small1 = tbSmalls.count(comps[0])
+                else:
+                    small1 = tbSmall.count(comps[0][0])
             if isinstance(comps[1], dict):
                 if comps[1]['format'] == '左右':
                     small2 = tbSmall.count(comps[1]['components'][0]) and tbSmall.count(comps[1]['components'][1])
             else:
-                small2 = tbSmall.count(comps[1][0])
+                if comps[1][1:2] == '-':
+                    small2 = tbSmalls.count(comps[1])
+                else:
+                    small2 = tbSmall.count(comps[1][0])
             
-            if comps[0] == '一' or comps[1] == '一':
-                ratio = '(0)'
+            if isinstance(comps[0], str) and tbZero.count(comps[0]):
+                if small2:
+                    ratio = '(1：2)' 
+                else:
+                    ratio = '(0：1)' 
+            elif isinstance(comps[1], str) and bZero.count(comps[1]):
+                if small1:
+                    ratio = '(2：1)' 
+                else:
+                    ratio = '(1：0)'
             elif small1 == small2:
                 ratio = '(1：1)'
             elif small1:
@@ -84,14 +115,14 @@ def genGlyphTab(file = 'glyph.json'):
             elif small2:
                 ratio = '(2：1)'
         elif format == '上中下':
-            small1 = isinstance(comps[0], str) and tbSmall.count(comps[0][0])
-            small2 = isinstance(comps[1], str) and tbSmall.count(comps[1][0])
-            small3 = isinstance(comps[2], str) and tbSmall.count(comps[2][0])
+            small1 = isinstance(comps[0], str) and tbSmall.count(comps[0][0]) + tbZero.count(comps[0][0])
+            small2 = isinstance(comps[1], str) and tbSmall.count(comps[1][0]) + tbZero.count(comps[1][0])
+            small3 = isinstance(comps[2], str) and tbSmall.count(comps[2][0]) + tbZero.count(comps[2][0])
 
             if small1 == small2 and small2 == small3:
                 ratio = '(1：1：1)'
             else:
-                rn = { True: 1, False: 2 }
+                rn = { 2: 0, 1: 1, 0: 2 }
                 ratio = '(%d：%d：%d)' % (rn[small1], rn[small2], rn[small3])
         elif format == '上三包围':
             ratio = '(v1：1h1：2)'
@@ -104,15 +135,16 @@ def genGlyphTab(file = 'glyph.json'):
         
         return ratio
 
-    def recursionQuery(char, format, comps, command):
+    def recursionQuery(char, format, pFormat, comps, command):
         ratio = ratioToFormat(format, comps)
+        format = pFormat + format
 
         for i in range(0, len(comps)):
             c = comps[i]
             if c == None:
                 continue
             if isinstance(c, dict):
-                recursionQuery(char, '%s%s%d%s' % (format, ratio, i, c['format']), c['components'], command)
+                recursionQuery(char, c['format'], '%s%s%d' % (format, ratio, i), c['components'], command)
                 continue
 
             cmd = command
@@ -127,6 +159,7 @@ def genGlyphTab(file = 'glyph.json'):
                 c = ch._jiegou_dict[c]['source']
 
             fmt = format + ratio + str(i) + cmd
+
             content = glyphTab.setdefault(c, dict())
             if fmt in content:
                 content[fmt] += char
@@ -135,9 +168,12 @@ def genGlyphTab(file = 'glyph.json'):
 
     for char, attrs in ch._jiegou_dict.items():
         if attrs["format"] == '单体':
-            glyphTab.setdefault(char, dict())['单体'] = char
+            if 'source' in attrs:
+                glyphTab.setdefault(attrs['source'], dict())['>'+char] = char
+            else:
+                glyphTab.setdefault(char, dict())['单体'] = char
         else:
-            recursionQuery(char, attrs['format'], attrs['components'], '')
+            recursionQuery(char, attrs['format'], '', attrs['components'], '')
 
     def recursionQueryFormat(format, comps, command):
         ratio = ratioToFormat(format, comps)
@@ -167,6 +203,8 @@ def genGlyphTab(file = 'glyph.json'):
         if len(attr) > 1:
             for comp, fmt in attr:
                 del glyphTab[comp][fmt]
+                if len(glyphTab[comp]) == 0:
+                    del glyphTab[comp]
             
             count = 0
             for c in ch._jiegou_dict[req[0]]['components']:
@@ -175,10 +213,10 @@ def genGlyphTab(file = 'glyph.json'):
                 else:
                     count += 1
                     
-            unNeed = '单体'
+            need = '单体'
             if count != len(attr):
-                unNeed = '-' + ''.join([s[0] for s in attr])
-            glyphTab.setdefault(req[0], {})[unNeed] = req
+                need = ''.join([s[0] for s in attr])
+            glyphTab.setdefault(req[0], {})[need] = req
 
     temp = set()
     for attr in glyphTab.values():
@@ -201,12 +239,18 @@ def genGlyphFiles(path='glyph', tabFile='glyph.json'):
 
     if not os.path.exists(path):
         os.mkdir(path)
+    oldFiles = os.listdir(path)
 
+    fileList = set()
     for comp, attr in ch.loadJson(tabFile).items():
         for fmt in attr.keys():
-            fileName = re.sub('>', '》', os.path.join(path, '%s：%s.svg' % (comp, fmt)))
-            tFile = open(fileName, "w", encoding='utf-8')
-            tFile.close()
+            fileName = re.sub('>', '》',  '%s：%s.svg' % (comp, fmt))
+            fileList.add(fileName)
+            if fileName not in oldFiles:
+                tFile = open(os.path.join(path, fileName), "w", encoding='utf-8')
+                tFile.close()
+    
+    return fileList
 
 if __name__ == '__main__':
     genGlyphTab()
